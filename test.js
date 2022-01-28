dd0694f04c15cacd91788cc284ed5d184175341f
dumbasshere <80169273+dumbasshere@users.noreply.github.com>
2022-01-28 17:15:41 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+
+function add(){
+    var input1=parseInt(document.getElementById("input1").value);
+    var input2=parseInt(document.getElementById("input2").value); 
+    let total=input1+input2;
+    alert(total)
+}
